e42189b9eb3444aabc91f9054f5222da3a2396b2
Yash Agarwal <yash23.agarwal@gmail.com>
2020-05-15 14:33:16 +0530

    beam search for hierarchial

--- beam_search.py
+++ beam_search.py
@@ -1,8 +1,7 @@
 import operator
 import os
 import re
-from main import args, data_without_types, data_with_type, data_entity_composite
-from data_utils import load_text_dataset, load_entity_composite_dataset
+from main import args
 import torch
 import torch.nn as nn
 from queue import PriorityQueue
@@ -15,47 +14,8 @@ def idx2word(idx, corpus):
 def word2idx(word, corpus):
     return corpus.dictionary.word2idx[word]
 
-def get_type(word, corpus, index = False):
-    if index:
-        word_ori = idx2word(word, corpus_entity_composite)
-        if word_ori in all_type_entities:
-            word_type = entity_type[word_ori]
-            return word2idx(word_type, corpus)
-        else:
-            return word
-    else:
-        if word in all_type_entities:
-            return entity_type[word]
-        else:
-            return word
-
-entity_type_folder = "all_data/recipe_data_new/types/"
-
-corpus_ori = load_text_dataset(data_without_types)
-corpus_awd_lstm = load_text_dataset(data_without_types)
-corpus_type = load_text_dataset(data_with_type)
-corpus_entity_composite = load_entity_composite_dataset(data_entity_composite)
-
-
 EOS_TOKEN = '<eos>'
-LogSoftmax = nn.LogSoftmax(dim=1)
 Softmax = nn.Softmax(dim=1)
-types = [file_name[:-4] for file_name in os.listdir(entity_type_folder)]
-type_indexes = list(map(lambda x : word2idx(x, corpus_type), types))
-
-
-regex = re.compile(".*?\((.*?)\)")
-all_type_entities = set()
-type_to_entites_dict = {}
-entity_type = {}
-for file_name in os.listdir(entity_type_folder):
-    f = open(entity_type_folder + file_name, "r")
-    entities = list(map(lambda x : x.lower().strip(), f.readlines()))
-    all_type_entities.update(entities)
-    type_to_entites_dict[file_name[:-4]] = entities
-    for entity in entities:
-        entity_type[entity] = file_name[:-4]
-
 device = "cuda:0" if args["cuda"] else "cpu"
 
 class BeamSearchNode(object):
@@ -96,7 +56,7 @@ def beam_search(model, corpus, hidden, initial_sentence):
     if model.is_attention_model():
         model.reset_last_layer()
     beam_width = 15
-    topk = 1
+    topk = 8
     for word in initial_sentence:
         output, hidden = get_next_word(model, corpus, word, hidden, False)
 
@@ -123,7 +83,7 @@ def beam_search(model, corpus, hidden, initial_sentence):
     qsize += len(next_nodes) - 1
 
     while True:
-        if qsize > 10000: break
+        if qsize > 20000: break
         score, n = nodes.get()
         new_word = n.wordid
         hidden = n.h
@@ -144,252 +104,6 @@ def beam_search(model, corpus, hidden, initial_sentence):
         for new_k in range(beam_width):
             decoded_t = indexes[0][new_k].view(1, -1)
             log_p = log_prob[0][new_k].item()
-
-            node = BeamSearchNode(hidden, n, decoded_t, n.logp + log_p, n.leng + 1)
-            score = -node.eval()
-            next_nodes.append((score, node))
-
-        for i in range(len(next_nodes)):
-            score, nn = next_nodes[i]
-            nodes.put((score, nn))
-        qsize += len(next_nodes) - 1
-
-    if len(endnodes) == 0:
-        endnodes = [nodes.get() for _ in range(topk)]
-
-    utterances = []
-    for score, n in sorted(endnodes, key=operator.itemgetter(0)):
-        utterance = []
-        utterance.append(n.wordid)
-        while n.prevNode != None:
-            n = n.prevNode
-            utterance.append(n.wordid)
-
-        utterance = utterance[::-1]
-        utterances.append(utterance)
-
-    return utterances
-
-def get_next_word_entity_composite(model, corpus, word_with_type, hidden, isIndex=True):
-    model_entity_composite, model_type = model
-    corpus_entity_composite, corpus_type = corpus
-    word, word_type = word_with_type
-    hidden_entity_composite, hidden_type = hidden
-    #type model prediction
-    output_type, new_hidden_type = get_next_word(model_type, corpus_type, word_type, hidden_type, isIndex)
-    output_type = output_type.view(-1)
-
-    # entity_composite prediction
-    if not isIndex:
-        word = word2idx(word, corpus_entity_composite)
-        word_type = word2idx(word_type, corpus_entity_composite)
-
-    #  model_entity_composite.eval()
-    output_entity_composite, new_hidden_entity_composite = model_entity_composite(torch.LongTensor([word]).view(1, -1), torch.LongTensor([word_type]).view(1, -1), hidden_entity_composite)
-    output_entity_composite = Softmax(output_entity_composite).view(-1)
-
-    # combined prediction
-    type_prob_sum = {}
-    for t in types:
-        p = 0.0
-        for entity in type_to_entites_dict[t]:
-            idx = word2idx(entity, corpus_entity_composite)
-            p += output_entity_composite[idx].data
-        type_prob_sum[t] = p
-    other_prob_sum = 1.0 - sum([prob_sum for _,prob_sum in type_prob_sum.items()])
-
-    probs = []
-    all_type_prob = sum([output_type[type_idx].data for type_idx in type_indexes])
-
-    #  probs = torch.zeros(len(corpus_ori.dictionary))
-    #  probs = (1-all_type_prob)/other_prob_sum*output_entity_composite[:len(corpus_ori.dictionary)]
-    probs = (1-all_type_prob)*output_entity_composite[:len(corpus_ori.dictionary)]
-
-    for word in all_type_entities:
-        idx = word2idx(word, corpus_ori)
-        p_type = output_type[get_type(idx, corpus_type, True)].data
-        type_of_word = entity_type[word]
-        p_entity_composite = output_entity_composite[idx].data/type_prob_sum[type_of_word]
-        probs[idx] = p_type * p_entity_composite
-
-    #  for idx, word in enumerate(corpus_ori.dictionary.idx2word):
-    #      if word in all_type_entities:
-    #          p_type = output_type[get_type(idx, corpus_type, True)].data
-    #          type_of_word = entity_type[word]
-    #          p_entity_composite = output_entity_composite[idx].data/type_prob_sum[type_of_word]
-    #          prob = p_type * p_entity_composite
-    #      else:
-    #          p_entity_composite = output_entity_composite[idx].data
-    #          prob = (1 - all_type_prob)*p_entity_composite
-    #      probs.append(prob)
-    return torch.Tensor(probs).view(1, -1), (new_hidden_entity_composite, new_hidden_type)
-
-
-def beam_search_entity_composite(model, corpus, hidden, initial_sentence):
-    if model[0].is_attention_model():
-        model[0].reset_last_layer()
-    if model[1].is_attention_model():
-        model[1].reset_last_layer()
-
-    beam_width = 10
-    topk = 8
-    for word in initial_sentence:
-        output, hidden = get_next_word_entity_composite(model, corpus, word, hidden, False)
-
-    endnodes = []
-    number_required = min((topk + 1), topk - len(endnodes))
-    nodes = PriorityQueue()
-    qsize = 0
-
-    output = output.squeeze()
-
-    #  output = torch.log(output)
-    #  log_prob, indexes = torch.topk(output, beam_width)
-    #  p = output/sum(output)
-    #  print(sum(p))
-    indexes = torch.multinomial(output, beam_width)
-    #  print(indexes)
-    #  print(output.data[indexes.tolist()])
-    log_prob = torch.log(output.data[indexes.tolist()])
-    #  print(log_prob)
-    next_nodes = []
-
-    for new_k in range(beam_width):
-        decoded_t = indexes[new_k].view(1, -1)
-        decoded_type_idx = get_type(decoded_t, corpus_entity_composite, True)
-        log_p = log_prob[new_k].item()
-
-        node = BeamSearchNode(hidden, None, torch.LongTensor([decoded_t, decoded_type_idx], device = device), 0, 1)
-        score = -node.eval()
-        next_nodes.append((score, node))
-
-    for i in range(len(next_nodes)):
-        score, nn = next_nodes[i]
-        nodes.put((score, nn))
-    qsize += len(next_nodes) - 1
-
-    while True:
-        if qsize > 10000: break
-        score, n = nodes.get()
-        new_word = n.wordid
-        hidden = n.h
-        if new_word[0].data == word2idx(EOS_TOKEN, corpus_ori) and n.prevNode != None:
-            endnodes.append((score, n))
-            if len(endnodes) >= number_required:
-                break
-            else:
-                continue
-
-        output, hidden = get_next_word_entity_composite(model, corpus, new_word.view(-1), hidden)
-        output = output.squeeze()
-        #  output = torch.log(output)
-        #  log_prob, indexes = torch.topk(output, beam_width)
-        #  p = output/sum(output)
-        indexes = torch.multinomial(output, beam_width)
-        log_prob = torch.log(output.data[indexes.tolist()])
-        next_nodes = []
-
-        for new_k in range(beam_width):
-            decoded_t = indexes[new_k].view(1, -1)
-            decoded_type_idx = get_type(decoded_t, corpus_entity_composite, True)
-            log_p = log_prob[new_k].item()
-
-            node = BeamSearchNode(hidden, n, torch.LongTensor([decoded_t, decoded_type_idx], device = device), n.logp + log_p, n.leng + 1)
-            score = -node.eval()
-            next_nodes.append((score, node))
-
-        for i in range(len(next_nodes)):
-            score, nn = next_nodes[i]
-            nodes.put((score, nn))
-        qsize += len(next_nodes) - 1
-
-    if len(endnodes) == 0:
-        endnodes = [nodes.get() for _ in range(topk)]
-
-    utterances = []
-    for score, n in sorted(endnodes, key=operator.itemgetter(0)):
-        utterance = []
-        utterance.append(n.wordid[0])
-        while n.prevNode != None:
-            n = n.prevNode
-            utterance.append(n.wordid[0])
-
-        utterance = utterance[::-1]
-        utterances.append(utterance)
-
-    return utterances
-
-
-def get_next_word_combined(model, corpus, word, hidden, isIndex = True):
-    corpus_entity_composite, corpus_type = corpus
-    if isIndex:
-        word_type = get_type(idx2word(word, corpus_entity_composite), corpus_type, False)
-        word_idx = word
-        word_type_idx = word2idx(word_type, corpus_type)
-    else:
-        word_type = get_type(word, corpus_type, False)
-        word_idx = word2idx(word, corpus_entity_composite)
-        word_type_idx = word2idx(word_type, corpus_type)
-
-    output, hidden = model(torch.LongTensor([word_idx]).view(1, -1), torch.LongTensor([word_type_idx]).view(1, -1), hidden)
-    output = Softmax(output)
-    return output, hidden
-
-def beam_search_combined(model, corpus, hidden, initial_sentence):
-    if model.is_attention_model():
-        model.reset_last_layer()
-    beam_width = 15
-    topk = 5
-    output = torch.rand(1, 54769)
-    for word in initial_sentence:
-        output, hidden = get_next_word_combined(model, corpus, word, hidden, False)
-
-    endnodes = []
-    number_required = min((topk + 1), topk - len(endnodes))
-    nodes = PriorityQueue()
-    qsize = 0
-
-    output = output.squeeze()
-    output = torch.log(output)
-    log_prob, indexes = torch.topk(output, beam_width)
-    next_nodes = []
-
-    for new_k in range(beam_width):
-        decoded_t = indexes[new_k].view(1, -1)
-        log_p = log_prob[new_k].item()
-
-        node = BeamSearchNode(hidden, None, decoded_t, 0, 1)
-        score = -node.eval()
-        next_nodes.append((score, node))
-
-    for i in range(len(next_nodes)):
-        score, nn = next_nodes[i]
-        nodes.put((score, nn))
-    qsize += len(next_nodes) - 1
-
-    while True:
-        if qsize > 20000: break
-        score, n = nodes.get()
-        new_word = n.wordid
-        hidden = n.h
-
-        if new_word == word2idx(EOS_TOKEN, corpus[0]) and n.prevNode != None:
-            endnodes.append((score, n))
-
-            if len(endnodes) >= number_required:
-                break
-            else:
-                continue
-
-        output, hidden = get_next_word_combined(model, corpus, new_word.view(1, 1), hidden)
-        output = output.squeeze()
-        output = torch.log(output)
-        log_prob, indexes = torch.topk(output, beam_width)
-        next_nodes = []
-
-        for new_k in range(beam_width):
-            decoded_t = indexes[new_k].view(1, -1)
-            log_p = log_prob[new_k].item()
 
             node = BeamSearchNode(hidden, n, decoded_t, n.logp + log_p, n.leng + 1)
             score = -node.eval()
--- generate_recipe.py
+++ generate_recipe.py
@@ -1,68 +1,30 @@
 import torch
-from beam_search import corpus_entity_composite, corpus_type, corpus_awd_lstm, corpus_ori
-from beam_search import beam_search, beam_search_entity_composite
-from beam_search import get_type, word2idx, idx2word
-from main import args, model_awd_lstm_save_path, model_type_save_path, model_entity_composite_save_path
-from awd_lstm.build_model import get_model as get_rnn_model, model_load
-from entity_composite.build_model import get_model as get_entity_composite_model
+from data_utils import load_hierarchial_corpus
+from main import args, data_hierarchial_path, model_type_save_path, model_type_to_subtype_save_path, model_subtype_to_word_save_path, model_hierarchial_save_path
+from rnn_model.build_model import get_model as get_rnn_model
+from double_input_rnn_model.build_model import get_model as get_double_input_rnn_model
+from hierarchial_model.build_model import get_model as get_hierarchial_model
+from rnn_model.utils import model_load
+from beam_search import beam_search, idx2word
 
+device = "cuda:0" if args["cuda"] else "cpu"
 
 with torch.no_grad():
-    #  model_awd_lstm, _, _ = get_rnn_model(corpus_awd_lstm, args)
-    #  model_awd_lstm_state_dict, _, _ = model_load(model_awd_lstm_save_path, "cpu")
-    #  model_awd_lstm.load_state_dict(model_awd_lstm_state_dict)
+    corpus_hierarchial = load_hierarchial_corpus(data_hierarchial_path)
+    model_type, _, _ = get_rnn_model(corpus_hierarchial, args)
+    model_type_to_subtype, _, _ = get_double_input_rnn_model(corpus_hierarchial, args)
+    model_subtype_to_word, _, _ = get_double_input_rnn_model(corpus_hierarchial, args)
 
-    #  initial_sentence = "preheat the oven . mix flour and water in a bowl .".split(' ')
-    #  hidden = model_awd_lstm.init_hidden(1)
-    #  search = [idx2word(word.item(), corpus_awd_lstm) for word in beam_search(model_awd_lstm, corpus_awd_lstm, hidden, initial_sentence)[0]]
-    #  print(search)
+    model_hierarchial, _, _ = get_hierarchial_model(corpus_hierarchial, model_subtype_to_word, model_type_to_subtype, model_type, args)
+    model_hierarchial_state_dict, _, _ = model_load(model_hierarchial_save_path, device)
+    model_hierarchial.load_state_dict(model_hierarchial_state_dict)
 
-
-    model_type, _, _ = get_rnn_model(corpus_type, args)
-    model_type_state_dict, _, _ = model_load(model_type_save_path, "cpu")
-    model_type.load_state_dict(model_type_state_dict)
-
-    initial_sentence = "preheat the oven . mix flour and water in a bowl .".split(' ')
-    hidden = model_type.init_hidden(1)
-    search = [idx2word(word.item(), corpus_awd_lstm) for word in beam_search(model_type, corpus_awd_lstm, hidden, initial_sentence)[0]]
+    initial_sentence = 'season chicken with salt and pepper .'
+    print()
+    print("initial_sentence : ", initial_sentence.replace('_', ' '))
+    print()
+    initial_sentence = initial_sentence.split()
+    hidden_hierarchial = model_hierarchial.init_hidden(1)
+    search = ' '.join([idx2word(word, corpus_hierarchial) for word in beam_search(model_hierarchial, corpus_hierarchial, hidden_hierarchial, initial_sentence)[-1]])
+    search = search.replace('_', ' ')
     print(search)
-
-    #  model_entity_composite, _, _ = get_entity_composite_model(corpus_entity_composite, args)
-    #  model_entity_composite_state_dict, _, _ = model_load(model_entity_composite_save_path, "cpu")
-    #  model_entity_composite.load_state_dict(model_entity_composite_state_dict)
-    #
-    #
-    #  model = (model_entity_composite, model_type)
-    #  hidden = (model_entity_composite.init_hidden(1), model_type.init_hidden(1))
-    #  corpus = (corpus_entity_composite, corpus_type)
-    #
-    #  #  print(get_type("the"))
-    #
-    #  print("corpus_ori : ", len(corpus_ori.dictionary))
-    #  print("corpus_type : ", len(corpus_type.dictionary))
-    #  print("corpus_entity_composite : ", len(corpus_entity_composite.dictionary))
-    #
-    #  initial_sentence = "melt butter over moderate heat in a medium stockpot or dutch oven .".split(' ')
-    #  initial_sentence = [(word, get_type(word)) for word in initial_sentence]
-    #  search = [idx2word(word.item(), corpus_awd_lstm) for word in beam_search_entity_composite(model, corpus, hidden, initial_sentence)[0]]
-    #  print(search)
-
-
-    # Generate output file
-
-    #  test_file = open("data_without_type/train.txt", "r")
-    #  test_recipes = test_file.readlines()[50:100]
-    #
-    #  output_file = open("generated_recipes.txt", "w")
-    #
-    #  for recipe in test_recipes:
-    #      first_sentence = recipe.split('.')[0] + '.'
-    #      hidden = model_awd_lstm.init_hidden(1)
-    #      search = [idx2word(word.item(), corpus_awd_lstm) for word in beam_search(model_awd_lstm, corpus_awd_lstm, hidden, first_sentence)[0]]
-    #      if search[0] == '<eos>':
-    #          search = search[1:]
-    #      generated_recipe = " ".join(search)
-    #      output_file.write("-"*40 + '\n\n')
-    #      output_file.write("INPUT : " + first_sentence + "\n\n")
-    #      output_file.write("OUTPUT : " + generated_recipe + '\n')
-    #      output_file.write("\n\n")
